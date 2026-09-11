7164f7a06be80c5837553d6dfbcc462653c39799
yas3in <jastinafa2016@gmail.com>
2026-09-11 22:02:03 +0330

    chore: normalize quotes in app configs

--- shop/addresses/apps.py
+++ shop/addresses/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class AddressesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'addresses'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "addresses"
+    verbose_name = "آدرس‌ها"
