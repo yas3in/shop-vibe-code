a79067f663a98f393cd2c0ac7082d1bcefeeb714
yas3in <jastinafa2016@gmail.com>
2026-09-11 22:10:10 +0330

    fix: space in urls

--- shop/config/urls.py
+++ shop/config/urls.py
@@ -5,16 +5,24 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("accounts/", include("accounts.urls.web")),
     path("accounts/panel/", include("accounts.urls.panel")),
+
     path("addresses/", include("addresses.urls.web")),
     path("addresses/panel/", include("addresses.urls.panel")),
+
     path("catalog/", include("catalog.urls.web")),
     path("panel/catalog/", include("catalog.urls.panel")),
+
     path("orders/", include("orders.urls.web")),
     path("orders/panel/", include("orders.urls.panel")),
+
     path("payments/", include("orders.urls.payment")),
+
     path("", include("core.urls.web")),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+        )
