b0b85b692bf65d5ede834cf187304d8b743df6b5
yas3in <jastinafa2016@gmail.com>
2026-09-11 22:01:44 +0330

    refactor: drop app_name from front urlconfs

--- shop/accounts/urls/web.py
+++ shop/accounts/urls/web.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from accounts.views import front
 
-app_name = 'accounts'
 
 urlpatterns = [
     path('login/', front.login_view, name='login'),
--- shop/core/urls/web.py
+++ shop/core/urls/web.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from core.views import front
 
-app_name = 'core'
-
 urlpatterns = [
     path('', front.home, name='home'),
     path('contact/', front.contact, name='contact'),
